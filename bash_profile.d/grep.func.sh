c2f599c5484d1fb43fe7ab279bb020bc3869d8a3
Habzy <habzyhs@gmail.com>
2014-07-22 10:44:28 +0800

    Update for mac and for grep py

--- bash_profile.d/grep.func.sh
+++ bash_profile.d/grep.func.sh
@@ -13,6 +13,16 @@ function xgrep()
     find . -type f -name "*\.xml" -print0 | xargs -0 grep --color -n "$@"
 }
 
+function pygrep()
+{
+    find . -type f -name "*\.py" -print0 | xargs -0 grep --color -n "$@"
+}
+
+function shgrep()
+{
+    find . -type f -name "*\.sh" -print0 | xargs -0 grep --color -n "$@"
+}
+
 function mkgrep()
 {
     find . -type f \( -name "*\.mk*" -or -iname "makefile" \) -print0 | xargs -0 grep --color -n "$@"
--- bash_profile.d/path-env.sh
+++ bash_profile.d/path-env.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 #Habzy Huang<habzyhs@gmail.com>
-export PATH=$PATH:/cygdrive/c/Program\ Files\ \(x86\)/Notepad++/
+
+#For Windows:
+#export PATH=$PATH:/cygdrive/c/Program\ Files\ \(x86\)/Notepad++/
+
+
+#For Mac OS X :
+
